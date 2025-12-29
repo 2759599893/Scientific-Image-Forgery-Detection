fc084fbd9701a54f040fe97cba10f286f3446564
liuyangyang <133761981+2759599893@users.noreply.github.com>
2025-12-29 12:20:52 +0800

    Update OpenNpy.py

--- OpenNpy.py
+++ OpenNpy.py
@@ -1,9 +1,10 @@
 import numpy as np
 
-# 替换 'your_file.npy' 为你的文件路径
+# 替换文件路径
 data = np.load('D:/InfSec/Data/recodai-luc-scientific-image-forgery-detection/train_masks/90.npy')
 
 # 现在 'data' 就是一个 NumPy 数组，你可以像操作普通数组一样操作它
 print(data.shape)  # 查看数组的形状/维度
 print(data.dtype)  # 查看数组的数据类型
+
 print(data)    # 查看前5个元素
